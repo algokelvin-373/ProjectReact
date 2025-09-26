99e33177631da7bf8d21551ad9c30580410fdd91
algokelvin-373 <kelvinht373@gmail.com>
2025-09-26 11:21:59 +0700

    [Weixin Train] UI Login

--- weixin-train/src/pages/index/index.jsx
+++ weixin-train/src/pages/index/index.jsx
@@ -1,45 +1,18 @@
-import { Component } from "react";
-import { connect } from "react-redux";
-import { View, Button, Text } from "@tarojs/components";
+import { View, Text } from "@tarojs/components";
+import Taro, { useLoad } from "@tarojs/taro";
 
-import { add, minus, asyncAdd } from "../../actions/counter";
+export default function Index() {
+  useLoad(() => {
+    setTimeout(() => {
+      Taro.reLaunch({
+        url: "/pages/Login/index",
+      });
+    }, 2000);
+  });
 
-import "./index.scss";
-
-@connect(
-  ({ counter }) => ({
-    counter,
-  }),
-  (dispatch) => ({
-    add() {
-      dispatch(add());
-    },
-    dec() {
-      dispatch(minus());
-    },
-    asyncAdd() {
-      dispatch(asyncAdd());
-    },
-  })
-)
-class Index extends Component {
-  componentWillReceiveProps(nextProps) {
-    console.log(this.props, nextProps);
-  }
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  render() {
-    return (
-      <View className="flex justify-center items-center w-full h-screen bg-[#1A2B88]">
-        <Text className="text-white text-8xl font-bold">大家好</Text>
-      </View>
-    );
-  }
+  return (
+    <View className="flex justify-center items-center w-full h-screen bg-[#1A2B88]">
+      <Text className="text-white text-8xl font-bold">大家好</Text>
+    </View>
+  );
 }
-
-export default Index;
