24fe3cd477fb98af30b221a31aa18d65658ea447
algokelvin-373 <kelvinht373@gmail.com>
2025-09-26 11:09:24 +0700

    [Weixin Train] Splash Screen

--- weixin-train/src/pages/index/index.jsx
+++ weixin-train/src/pages/index/index.jsx
@@ -1,46 +1,45 @@
-import { Component } from 'react'
-import { connect } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
+import { Component } from "react";
+import { connect } from "react-redux";
+import { View, Button, Text } from "@tarojs/components";
 
-import { add, minus, asyncAdd } from '../../actions/counter'
+import { add, minus, asyncAdd } from "../../actions/counter";
 
-import './index.scss'
+import "./index.scss";
 
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
+@connect(
+  ({ counter }) => ({
+    counter,
+  }),
+  (dispatch) => ({
+    add() {
+      dispatch(add());
+    },
+    dec() {
+      dispatch(minus());
+    },
+    asyncAdd() {
+      dispatch(asyncAdd());
+    },
+  })
+)
 class Index extends Component {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps);
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <View>
-          <Text className='font-bold'>Hello, World</Text>
-        </View>
+      <View className="flex justify-center items-center w-full h-screen bg-[#1A2B88]">
+        <Text className="text-white text-8xl font-bold">大家好</Text>
       </View>
-    )
+    );
   }
 }
 
-export default Index
-
+export default Index;
