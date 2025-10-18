e26c9a7afa3d3461ec600c750f42048b482c4447
algokelvin-373 <kelvinht373@gmail.com>
2025-10-18 18:11:41 +0700

    [1.0.0][Tailwindcss] Update

--- tailwindcss-test/src/App.jsx
+++ tailwindcss-test/src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
