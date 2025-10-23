dcb9f9201c5d824320dd4508af554555f3bc6c28
algokelvin-373 <kelvinht373@gmail.com>
2025-10-23 15:44:29 +0700

    [AI Services] Create Page Splash and Set First Page

--- ai-services/src/main.jsx
+++ ai-services/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css"; // ← HARUS ADA
+import Splash from "./pages/Splash.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Splash />
   </React.StrictMode>
 );
