7f5ead97d7e020d959451d84629456b15d1b79f0
algokelvin-373 <kelvinht373@gmail.com>
2025-11-26 14:31:28 +0700

    [1.0.0][CV Portfolio] Init Project

---
+++ cv-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-blue-600 text-white p-6 rounded-xl shadow-lg">
+        <h1 className="text-2xl font-bold">✅ Tailwind Berhasil!</h1>
+        <p className="mt-2">
+          Jika kamu melihat ini dengan latar biru → SEMUA BERHASIL!
+        </p>
+      </div>
+    </div>
+  );
+}
